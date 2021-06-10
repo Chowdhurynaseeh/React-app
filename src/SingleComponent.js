5c41ff69f2715365a7e2f2541e28b4b1fc78e6dc
chowdhurynaseeh <chowdhurynasee@gmail.com>
2021-06-10 21:01:24 +0600

    A React project

---
+++ src/SingleComponent.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const SingleComponent = (props) => {
+    console.log(props)
+    return (
+        <div className='comment'>
+            <a href="/" className='avatar'>
+                <img src={ props.propic } alt="profile picture"/>
+            </a>
+                
+            <div className='content'>
+                <a href="/" className="author">
+                    { props.name }
+                </a>
+                <div className="metadata">
+                    <span className="data">
+                        { props.date }
+                    </span>
+                </div>
+                <div className='text'>
+                    { props.text }
+                </div> 
+            </div>
+        </div>
+    )
+}
+
+export default SingleComponent;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import propic1 from './img/pic_1.jpg';
+import propic2 from './img/pic_2.png';
+import propic3 from './img/pic_3.jpeg';
+import SingleComponent from './SingleComponent';
+
+const App = () => {
+    return (
+        <div className='ui comments'>
+            <SingleComponent 
+                name='Alex' 
+                propic = { propic1 } 
+                date = 'today at 5:00 pm' 
+                text='This is amazing' 
+            /> 
+            <SingleComponent 
+                name='Babul' 
+                propic = { propic2 } 
+                date = 'yesterday at 3:00 pm' 
+                text='wow, wonderful!!' 
+            />
+            <SingleComponent 
+                name='Kabul' 
+                propic = { propic3 } 
+                date = 'today at 10:00 pm' 
+                text='Awesome!!' 
+            />
+        </div>
+    )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App />,
+    document.querySelector("#root")
+)
